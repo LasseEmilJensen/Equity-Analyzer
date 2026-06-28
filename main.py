fab588d550d7c45321bee5be3efa8444d299e7dc
lassejensen465 <lasse.jensen2@icloud.com>
2026-06-28 15:37:30 +0200

    Version 0.1 - Initial stock data retrieval

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Welcome to Equity Analyzer!")
+import yfinance as yf
+
+print("=" * 40)
+print("     Equity Analyzer")
+print("=" * 40)
+
+ticker = input("Enter ticker: ")
+
+stock = yf.Ticker(ticker)
+
+info = stock.info
+
+print("\nCompany:", info.get("longName"))
+print("Current Price:", info.get("currentPrice"))
+print("Currency:", info.get("currency"))
